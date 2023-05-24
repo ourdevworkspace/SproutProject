da83bbff25991ec9e6cf7053d31de82d7e4e5a6d
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-05-24 01:15:23 -0400

    Removing vite template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,html,tsx,ts}"
+    "./index.html",
+    "./src/**/*.{js,jsx,tsx,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy'],
+      },
+    },
+    colors: {
+      //Main Colors
+      brown: '#383634',
+      green: '#4c7c7d',
+      red: '#e45959',
+      ivory: '#edeae7'
+
+    },
+    screens: {
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... },
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      desktop: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
