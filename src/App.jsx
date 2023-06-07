8854b423566ecaf56d12c4ab624bae72012c259e
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-06-06 22:15:25 -0400

    passing simon data to the repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,74 @@
+import logo from './assets/images/logo.png'
+
+
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <h1 className="font-gilroy text-6xl text-brown">Sprout</h1>
+      {/*     <!-- LAYOUT --> */}
+      <div className="layout">
+        {/*    <!-- HEADER --> */}
+        <header className="header">
+          {/*         <!-- HEADER: LEFT --> */}
+          <div className="header__left">
+            {/*   <!-- HEADER: LEFT - LOGO --> */}
+            <a className="header__left--logo" href="./index.html">
+              <img src={logo} alt="Sprout" />
+            </a>
+            {/*  <!-- HEADER: LEFT - SEARCH --> */}
+            <form className="header__left--search">
+              <input type="text" placeholder="Search" />
+              <span className="material-symbols-outlined">search</span>
+            </form>
+            {/*  <!-- HEADER: LEFT - CATEGORIES --> */}
+            <div className="header__left--categories">
+              <span className="material-symbols-outlined">apps</span>
+              <p>Categories</p>
+            </div>
+          </div>
+          {/* <!-- HEADER: RIGHT --> */}
+          <div className="header__right">
+            {/*  <!-- HEADER: RIGHT - SEARCH --> */}
+            <button className="header__right--search">
+              <span className="material-symbols-outlined">search</span>
+            </button>
+            {/* <!-- HEADER: RIGHT - ACCOUNT --> */}
+            <button className="header__right--account">
+              <span className="material-symbols-outlined">account_circle</span>
+              <p>Agustín</p>
+              <span className="material-symbols-outlined">expand_more</span>
+            </button>
+            {/* <!-- HEADER: RIGHT - CART --> */}
+            <button className="header__right--cart">
+              <span className="material-symbols-outlined">local_mall</span>
+              <p>Cart: <span>6</span></p>
+            </button>
+            {/* <!-- HEADER: RIGHT - MENU --> */}
+            <button className="header__right--menu">
+              <span className="material-symbols-outlined">menu</span>
+            </button>
+          </div>
+        </header>
+        {/*  <!-- MAIN --> */}
+        <main className="main">
+          {/* <!-- BANNERS --> */}
+          <section className="banners">
+            {/* <!-- BANNERS: CONTAINER --> */}
+            <div className="banners__container">
+              {/* <!-- BANNER: LEFT --> */}
+              <div className="banner banner--left"></div>
+              {/*  <!-- BANNER: RIGHT --> */}
+              <div className="banner banner--right"></div>
+            </div>
+            {/* <!-- BANNERS: CONTROLS --> */}
+            <div className="banners__controls">
+              <div className="banners__controls--left"></div>
+              <div className="banners__controls--right"></div>
+            </div>
+          </section>
+        </main>
       </div>
     </>
   )
