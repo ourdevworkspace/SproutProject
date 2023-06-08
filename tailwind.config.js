75422cff0b6a9c2dbd14071fac237c8a3b2cbc72
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-06-08 01:55:47 -0400

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
       seaShell: "#f6f4f2",
       white: "#ffffff",
       dawn: "#a4a2a0",
-      
+      whiteBg:"#f4f2f0"
 
       //Other Colors
 
@@ -37,6 +37,20 @@ export default {
       desktop: '1280px',
       // => @media (min-width: 1280px) { ... }
     },
+    fontSize: {
+      xs:['.5rem', '1rem'],
+      sm: ['.75rem', '1rem'], // [fontSize, lineHeight] <- this format is used in this theme
+      base: ['1rem', '1.5rem'], // [fontSize, { lineHeight?, letterSpacing?, fontWeight? }] <- this one not(though it can be)
+      lg: ['1.2rem', '1.75rem'],
+      'xl': ['1.5rem', '2rem'],
+      '2xl': ['1.875rem', '2.25rem'],
+      '3xl':['2.25rem', '2.5rem'],
+      '4xl':['3rem', '1'],
+      '5xl':['3.75rem', '1'],
+      '6xl':['4.5rem','1'],
+      '7xl':['6rem', '1'],
+      '8xl':['8rem','1']
+    }
   },
   plugins: [],
 }
